43476e9b848b90a0228318ec01bc0fbbf2f6bbf8
nstdev0 <nst.dev0@gmail.com>
2026-02-08 22:20:37 -0500

    feat: Add Vercel adapter for Astro deployment configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    integrations: [react()],
-    output: 'static',
-    i18n: {
-        defaultLocale: "en",
-        locales: ["en", "es"],
-        routing: {
-            prefixDefaultLocale: false
-        }
-    }
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
+  output: 'static',
+
+  i18n: {
+      defaultLocale: "en",
+      locales: ["en", "es"],
+      routing: {
+          prefixDefaultLocale: false
+      }
+  },
+
+  adapter: vercel()
 });
