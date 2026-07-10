b49320cc5ebbc3b4839ea50f00cc8f08bbf2c238
nstdev0 <nst.dev0@gmail.com>
2026-07-09 20:27:19 -0500

    implemented smtp service on contact form

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,18 +1,1 @@
-import { defineAction } from "astro:actions";
-import { z } from "astro:schema";
-
-export const server = {
-  sendContact: defineAction({
-    accept: "form",
-    input: z.object({
-      email: z.string().email(),
-      message: z.string().min(5),
-    }),
-    handler: async (input) => {
-      console.log("Nuevo mensaje:", input);
-      await new Promise((r) => setTimeout(r, 1000));
-
-      return { success: true };
-    },
-  }),
-};
+export const server = {};
